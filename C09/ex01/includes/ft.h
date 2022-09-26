4df3b201ea127649fce31771048f99f193e5877c
diegogalanti <dggalanti@hotmail.com>
2022-09-26 13:27:12 +0200

    C09 ex02 using makefile

---
+++ C09/ex01/includes/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: digallar <digallar@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/22 16:16:18 by digallar          #+#    #+#             */
+/*   Updated: 2022/09/26 13:21:32 by digallar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
---
+++ C09/ex01/srcs/ft_putstr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: digallar <digallar@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/18 19:13:05 by digallar          #+#    #+#             */
+/*   Updated: 2022/08/19 12:53:19 by digallar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	iterator;
+	int	size;
+
+	iterator = 0;
+	size = 0;
+	while (str[iterator] != '\0')
+	{
+		iterator++;
+		size++;
+	}
+	write(1, str, size);
+}
